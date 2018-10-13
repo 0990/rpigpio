11299135d31a045da006145bff0035aa77c66546
xujialong <xjlong@outlook.com>
2018-10-13 11:21:34 +0800

    温湿度dht11

--- dht11/main.go
+++ dht11/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/MichaelS11/go-dht"
+)
 
+func main() {
+	err := dht.HostInit()
+	if err != nil {
+		fmt.Println("HostInit error:", err)
+		return
+	}
+
+	dht, err := dht.NewDHT("GPIO19", dht.Fahrenheit, "")
+	if err != nil {
+		fmt.Println("NewDHT error:", err)
+		return
+	}
+
+	humidity, temperature, err := dht.ReadRetry(11)
+	if err != nil {
+		fmt.Println("Read error:", err)
+		return
+	}
+
+	fmt.Printf("humidity:%v\n", humidity)
+	fmt.Printf("temperature:%v\n", temperature)
 }

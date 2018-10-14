7d6fbe03c90ad661f726da8688194cf4e93459b4
xujialong <xjlong@foxmail.com>
2018-10-14 12:17:26 +0800

    add pwm test

--- dht11/main.go
+++ dht11/main.go
@@ -7,26 +7,6 @@ import (
 )
 
 func main() {
-	//err := dht.HostInit()
-	//if err != nil {
-	//	fmt.Println("HostInit error:", err)
-	//	return
-	//}
-	//
-	//dht, err := dht.NewDHT("GPIO17", dht.Fahrenheit, "dht11")
-	//if err != nil {
-	//	fmt.Println("NewDHT error:", err)
-	//	return
-	//}
-	//
-	//humidity, temperature, err := dht.ReadRetry(11)
-	//if err != nil {
-	//	fmt.Println("Read error:", err)
-	//	return
-	//}
-	//
-	//fmt.Printf("humidity:%v\n", humidity)
-	//fmt.Printf("temperature:%v\n", temperature)
 	temperature, humidity, retried, err :=
 		dht.ReadDHTxxWithRetry(dht.DHT11, 17, true, 10)
 	if err != nil {
---
+++ rgb/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/stianeikeland/go-rpio"
+	"os"
+	"time"
+)
+
+func main() {
+	err := rpio.Open()
+	if err != nil {
+		os.Exit(1)
+	}
+	defer rpio.Close()
+
+	pin := rpio.Pin(19)
+
+	pin.Mode(rpio.Pwm)
+	pin.Freq(64000)
+	pin.DutyCycle(0, 32)
+
+	for i := 0; i < 5; i++ {
+		for i := uint32(0); i < 32; i++ {
+			pin.DutyCycle(i, 32)
+			time.Sleep(time.Second / 32)
+		}
+		for i := uint32(32); i > 0; i-- {
+			pin.DutyCycle(i, 32)
+		}
+	}
+}

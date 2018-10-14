0c568eaf3789c13e28112f4f7bdc32d9838d3bef
xujialong <xjlong@foxmail.com>
2018-10-14 13:00:53 +0800

    add pwm test

--- rgb/main.go
+++ rgb/main.go
@@ -17,15 +17,15 @@ func main() {
 
 	pin.Mode(rpio.Pwm)
 	pin.Freq(64000)
-	pin.DutyCycle(0, 32)
+	pin.DutyCycle(0, 255)
 
 	for i := 0; i < 5; i++ {
-		for i := uint32(0); i < 32; i++ {
-			pin.DutyCycle(i, 32)
-			time.Sleep(time.Second / 32)
+		for i := uint32(0); i < 255; i++ {
+			pin.DutyCycle(i, 255)
+			time.Sleep(time.Second / 255)
 		}
-		for i := uint32(32); i > 0; i-- {
-			pin.DutyCycle(i, 32)
+		for i := uint32(255); i > 0; i-- {
+			pin.DutyCycle(i, 255)
 		}
 	}
 }
